e24ba446df5d721d1b780b3284ae2b358fdcb9d0
Anzu <69132522+AnzuFTNW@users.noreply.github.com>
2022-01-16 03:23:11 +0100

    Add files via upload

---
+++ tl/src/screenshot.js
@@ -0,0 +1,29 @@
+// Define the function
+// to screenshot the div
+function doScreenShot() {
+  const tierlist = document.getElementById("tierlist");
+  const outputWindow = document.getElementById("output");
+
+  html2canvas(tierlist, { allowTaint: true }).then(function (canvas) {
+    outputWindow.appendChild(canvas);
+    outputWindow.style.display = "flex";
+  });
+}
+
+function closeWindow() {
+  const outputWindow = document.getElementById("output");
+  outputWindow.style.display = "none";
+  outputWindow.removeChild(outputWindow.lastChild);
+}
+
+function saveScreenshot() {
+  const tempLink = document.createElement("A");
+  const screenShot = document.querySelector("canvas");
+
+  tempLink.setAttribute("download", "tierlist.png");
+  tempLink.setAttribute(
+    "href",
+    screenShot.toDataURL("image/png").replace("image/png", "image/octet-stream")
+  );
+  tempLink.click();
+}
